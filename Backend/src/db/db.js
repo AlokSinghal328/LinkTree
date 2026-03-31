4f8a242a3323ad9d0972dd0138da5b2adbda8c45
Alok Singhal <aloksinghal328@gmail.com>
2026-03-31 14:42:48 +0530

    Set up backend project structure

---
+++ Backend/src/db/db.js
@@ -0,0 +1,12 @@
+const mongooose = require('mongoose');
+
+async function connectDB() {
+    try{
+        await mongooose.connect(process.env.MONGODB_URI);
+        console.log("Database connected successfully");
+
+    } catch (error) {
+        console.error("database connection error : ", error);
+        process.exit(1);
+    }
+}
